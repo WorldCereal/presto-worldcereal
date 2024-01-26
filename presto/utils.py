dc329ffe8a546615d1e243f32ff6255101a2a3e5
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-01-26 09:57:56 +0800

    Plot NDVI too

--- presto/utils.py
+++ presto/utils.py
@@ -280,10 +280,11 @@ def plot_results(
 
 def plot_spatial(spatial_preds: xr.Dataset, output_path: Path, to_wandb: bool = False):
     plt.clf()
-    _, axs = plt.subplots(ncols=3, figsize=(20, 4))
-    spatial_preds.ground_truth.plot(ax=axs[0], vim=0, vmax=1)
-    spatial_preds.prediction_0.plot(ax=axs[1], vim=0, vmax=1)
-    (spatial_preds.prediction_0 > 0.5).plot(ax=axs[2], vim=0, vmax=1)
+    _, axs = plt.subplots(ncols=4, figsize=(25, 4))
+    spatial_preds.ndvi.plot(ax=axs[0], vim=0, vmax=1)
+    spatial_preds.ground_truth.plot(ax=axs[1], vim=0, vmax=1)
+    spatial_preds.prediction_0.plot(ax=axs[2], vim=0, vmax=1)
+    (spatial_preds.prediction_0 > 0.5).plot(ax=axs[3], vim=0, vmax=1)
     plt.savefig(output_path, bbox_inches="tight")
     if to_wandb:
         import wandb
