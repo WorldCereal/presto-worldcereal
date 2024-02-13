51b5f198fd43f1d7895049f90a5393bc02502b94
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-02-13 15:25:07 +0800

    Don't change too many things at once

--- presto/eval.py
+++ presto/eval.py
@@ -34,9 +34,9 @@ SklearnStyleModel = Union[BaseEstimator, CatBoostClassifier]
 
 @dataclass
 class Hyperparams:
-    lr: float = 3e-4
+    lr: float = 2e-5
     max_epochs: int = 100
-    batch_size: int = 128
+    batch_size: int = 64
     patience: int = 3
     num_workers: int = 4
 
