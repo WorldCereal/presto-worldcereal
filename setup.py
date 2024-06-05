067baaf76c604587df83baffa66fd45f29a46fa4
Kristof Van Tricht <kristof.vantricht@vito.be>
2024-06-05 13:11:09 +0200

    alternative dependency management

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import List
+
 from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -6,6 +9,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
 long_description = "\n".join(
     [line for line in long_description.split("\n") if not line.startswith("<img")]
 )
+
+
+def load_dependencies(tag: str) -> List[str]:
+    """Load the dependencies from the specified requirements file."""
+    with open(Path(__file__).parent / f"requirements.{tag}.txt") as f:
+        requirements = [
+            line.strip() for line in f.readlines() if line.strip() and (line[0] != "#")
+        ]
+    return requirements
+
+
+# List all possible dependencies
+run_dependencies = load_dependencies("inference")
+train_dependencies = load_dependencies("training")
+dev_dependencies = load_dependencies("dev")
+
 
 setup(
     name="presto-worldcereal",
@@ -21,21 +40,11 @@ setup(
         "Operating System :: OS Independent",
     ],
     packages=["presto"] + [f"presto.{f}" for f in find_packages("presto")],
-    install_requires=[
-        "einops==0.6.0",
-        "h5py==3.6.0",
-        "matplotlib==3.6.2",
-        "numpy==1.23.5",
-        "torch==2.0",
-        "tqdm==4.64.1",
-        "webdataset==0.2.31",
-        "cropharvest==0.6.0",
-        "google-cloud-storage==2.2.1",
-        "earthengine-api==0.1.334",
-        "hurry.filesize==0.9",
-        "openmapflow[data]==0.2.2",
-        "rioxarray==0.13.1",
-    ],
-    python_requires=">=3.9",
+    install_requires=run_dependencies,
+    python_requires=">=3.8",
     include_package_data=True,
+    extras_require={
+        "train": train_dependencies,
+        "dev": dev_dependencies + train_dependencies,
+    },
 )
