ec45683dbe0659a234e76e34ed7b31b8431b1bc3
Kristof Van Tricht <kristof.vantricht@vito.be>
2024-06-07 10:38:11 +0200

    another go at lightweight inference

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import List
+
 from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -6,6 +9,21 @@ with open("README.md", "r", encoding="utf-8") as fh:
 long_description = "\n".join(
     [line for line in long_description.split("\n") if not line.startswith("<img")]
 )
+
+
+def load_dependencies(tag: str) -> List[str]:
+    """Load the dependencies from the specified requirements file."""
+    with open(Path(__file__).parent / f"requirements.{tag}.txt") as f:
+        requirements = [
+            line.strip() for line in f.readlines() if line.strip() and (line[0] != "-")
+        ]
+    return requirements
+
+
+# List all possible dependencies
+inference_dependencies = load_dependencies("inference")
+full_dependencies = load_dependencies("full")
+
 
 setup(
     name="presto-worldcereal",
@@ -21,13 +39,10 @@ setup(
         "Operating System :: OS Independent",
     ],
     packages=["presto"] + [f"presto.{f}" for f in find_packages("presto")],
-    install_requires=[
-        "einops==0.6.0",
-        "numpy==1.23.5",
-        "torch==2.0",
-        "tqdm==4.64.1",
-        "rioxarray==0.13.1",
-    ],
-    python_requires=">=3.9",
+    install_requires=inference_dependencies,
+    extras_require={
+        "full": inference_dependencies + full_dependencies,
+    },
+    python_requires=">=3.8",
     include_package_data=True,
 )
