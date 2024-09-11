c9ffa01c35343ed0463e238c138608265755c19e
Christina Butsko <cbutsko@cbutsko.vitovm.rscloud.vito.be>
2024-09-11 12:35:17 +0200

    updated test dataset to use new ong parquet format

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,9 @@
+from glob import glob
+
 import numpy as np
 import pandas as pd
 
-from presto.utils import data_dir
+from presto.utils import process_parquet
 
 # we will have a different number of maize labels for easier testing
 NUM_CROP_POINTS = 10
@@ -9,11 +11,16 @@ NUM_MAIZE_POINTS = 20
 
 
 def read_test_file() -> pd.DataFrame:
-    test_df = pd.read_parquet(data_dir / "worldcereal_testdf.parquet")[:20]
-    # this is to align the parquet file with the new parquet files
-    # shared in https://github.com/WorldCereal/presto-worldcereal/pull/34
+    parquet_file = "/vitodata/worldcereal/features/preprocessedinputs-monthly-nointerp/\
+worldcereal_training_data.parquet"
+    files = sorted(glob(f"{parquet_file}/**/*.parquet"))
+    f = files[7]
+    test_df_long = pd.read_parquet(f, engine="fastparquet")
+    test_df = process_parquet(test_df_long)
+    test_df.reset_index(inplace=True)
+
     test_df.rename(
-        {"catboost_prediction": "worldcereal_prediction"},
+        {"WORLDCOVER-LABEL-10m": "worldcereal_prediction"},
         axis=1,
         inplace=True,
     )
