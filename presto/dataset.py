fixed ndvi masking

presto/dataset.py
@@ -13,15 +13,9 @@ from einops import rearrange
 from pyproj import CRS, Transformer
 from torch.utils.data import Dataset
 
-from .dataops import (
-    BANDS,
-    BANDS_GROUPS_IDX,
-    MIN_EDGE_BUFFER,
-    NODATAVALUE,
-    NORMED_BANDS,
-    S1_S2_ERA5_SRTM,
-    DynamicWorld2020_2021,
-)
+from .dataops import (BANDS, BANDS_GROUPS_IDX, MIN_EDGE_BUFFER, NDVI_INDEX,
+                      NODATAVALUE, NORMED_BANDS, S1_S2_ERA5_SRTM, S2_RGB_INDEX,
+                      DynamicWorld2020_2021, S2_NIR_10m_INDEX)
 from .masking import BAND_EXPANSION, MaskedExample, MaskParamsNoDw
 from .utils import DEFAULT_SEED, data_dir, get_class_mappings, load_world_df
 
@@ -203,12 +197,20 @@ required {cls.NUM_TIMESTEPS}, got {len(timestep_positions)}"
             eo_data[:, BANDS.index(presto_val)] = values * idx_valid
             mask[:, IDX_TO_BAND_GROUPS[presto_val]] += ~idx_valid
 
+        # check if the visual bands mask is True
+        # or nir mask, and adjust the NDVI mask accordingly
+        mask[:, NDVI_INDEX] = np.logical_or(
+            mask[:, S2_RGB_INDEX],
+            mask[:, S2_NIR_10m_INDEX]
+            )
+
         return (cls.check(eo_data), mask.astype(bool), latlon, month, valid_month)
 
     def __getitem__(self, idx):
         # Get the sample
         row = self.df.iloc[idx, :]
         eo, mask_per_token, latlon, month, valid_month = self.row_to_arrays(row)
+
         mask_per_variable = np.repeat(mask_per_token, BAND_EXPANSION, axis=1)
         return (
             self.normalize_and_mask(eo),
@@ -360,6 +362,7 @@ class WorldCerealMaskedDataset(WorldCerealBase):
         eo, real_mask_per_token, latlon, month, valid_month = self.row_to_arrays(
             row, self.task_type, self.croptype_list, self.is_ssl
         )
+
         mask_eo, x_eo, y_eo, strat = self.mask_params.mask_data(
             self.normalize_and_mask(eo), real_mask_per_token
         )

tests/test_dataset.py
@@ -4,15 +4,11 @@ from unittest import TestCase
 import numpy as np
 import pandas as pd
 import torch
-
-from presto.dataops import NODATAVALUE, NUM_ORG_BANDS, NUM_TIMESTEPS
-from presto.dataset import (
-    WorldCerealBase,
-    WorldCerealInferenceDataset,
-    WorldCerealLabelledDataset,
-    WorldCerealMaskedDataset,
-    filter_remove_noncrops,
-)
+from presto.dataops import (NDVI_INDEX, NODATAVALUE, NUM_ORG_BANDS,
+                            NUM_TIMESTEPS, S2_RGB_INDEX, S2_NIR_10m_INDEX)
+from presto.dataset import (WorldCerealBase, WorldCerealInferenceDataset,
+                            WorldCerealLabelledDataset,
+                            WorldCerealMaskedDataset, filter_remove_noncrops)
 from presto.eval import WorldCerealEval
 from presto.masking import MaskParamsNoDw
 from presto.presto import Presto
@@ -148,6 +144,27 @@ class TestDataset(TestCase):
         output_list = WorldCerealLabelledDataset.multiply_list_length_by_float(input_list, 2.5)
         self.assertEqual(len(output_list), 25)
 
+    def test_mask_consistency(self):
+        df = read_test_file()
+        test_row = df.sample().iloc[0]
+        # make first 6 timesteps filled with NODATAVALUE
+        # expected behavior is that NDVI is masked out
+        # at the first six timesteps
+        valid_position = int(test_row["valid_position"])
+        for ts in range(valid_position-6, valid_position-3):
+            test_row[f"OPTICAL-B04-ts{ts}-10m"] = NODATAVALUE
+        for ts in range(valid_position-3, valid_position-1):
+            test_row[f"OPTICAL-B08-ts{ts}-10m"] = NODATAVALUE
+        test_row[f"OPTICAL-B04-ts{valid_position-1}-10m"] = NODATAVALUE
+        test_row[f"OPTICAL-B08-ts{valid_position-1}-10m"] = NODATAVALUE
+        
+        eo, mask, latlon, month, valid_month = WorldCerealBase.row_to_arrays(test_row)
+
+        self.assertTrue(set([0,1,2,5]) & set(np.where(mask[:, S2_RGB_INDEX])[0]))
+        self.assertTrue(set([3,4,5]) & set(np.where(mask[:, S2_NIR_10m_INDEX])[0]))
+        self.assertTrue(mask[:6, NDVI_INDEX].all())
+
+
     def test_balancing_croptype(self):
         finetune_classes = "CROPTYPE0"
         downstream_classes = "CROPTYPE9"
@@ -217,6 +234,9 @@ class TestDataset(TestCase):
         timestep_positions_augment2 = WorldCerealLabelledDataset.get_timestep_positions(
             row_d, augment=True, is_ssl=False
         )
+        timestep_positions_augment3 = WorldCerealLabelledDataset.get_timestep_positions(
+            row_d, augment=True, is_ssl=False
+        )
 
         timestep_positions_ssl1 = WorldCerealLabelledDataset.get_timestep_positions(
             row_d, augment=False, is_ssl=True
@@ -232,7 +252,10 @@ class TestDataset(TestCase):
             timestep_positions_base[-1]
             == (valid_position + (WorldCerealBase.NUM_TIMESTEPS // 2) - 1)
         )
-        self.assertTrue(timestep_positions_augment1[0] != timestep_positions_augment2[0])
+        self.assertTrue(np.unique([
+            timestep_positions_augment1[0],
+            timestep_positions_augment2[0],
+            timestep_positions_augment3[0]]).shape[0] > 1)
         self.assertTrue(timestep_positions_ssl1[0] != timestep_positions_ssl2[0])
         self.assertTrue(
             (valid_position not in timestep_positions_ssl1)