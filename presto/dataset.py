97ebb7d149b9745c0ae5dd83995980c061422a9f
Butsko Christina <butskoc@sasdsnode01.vito.local>
2024-12-11 09:14:25 +0100

    avoid pandas to_datetime for 12-timesteps case

--- presto/dataset.py
+++ presto/dataset.py
@@ -13,18 +13,9 @@ from einops import rearrange
 from pyproj import CRS, Transformer
 from torch.utils.data import Dataset
 
-from .dataops import (
-    BANDS,
-    BANDS_GROUPS_IDX,
-    MIN_EDGE_BUFFER,
-    NDVI_INDEX,
-    NODATAVALUE,
-    NORMED_BANDS,
-    S1_S2_ERA5_SRTM,
-    S2_RGB_INDEX,
-    DynamicWorld2020_2021,
-    S2_NIR_10m_INDEX,
-)
+from .dataops import (BANDS, BANDS_GROUPS_IDX, MIN_EDGE_BUFFER, NDVI_INDEX,
+                      NODATAVALUE, NORMED_BANDS, S1_S2_ERA5_SRTM, S2_RGB_INDEX,
+                      DynamicWorld2020_2021, S2_NIR_10m_INDEX)
 from .masking import BAND_EXPANSION, MaskedExample, MaskParamsNoDw
 from .utils import DEFAULT_SEED, data_dir, get_class_mappings, load_world_df
 
@@ -143,7 +134,7 @@ required {cls.NUM_TIMESTEPS}, got {len(timestep_positions)}"
         timestep_positions = cls.get_timestep_positions(row_d, augment=augment, is_ssl=is_ssl)
 
         if cls.NUM_TIMESTEPS == 12:
-            initial_start_date_position = pd.to_datetime(row_d["start_date"]).month
+            initial_start_date_position = datetime.strptime(row_d["start_date"], "%Y-%m-%d").month
         elif cls.NUM_TIMESTEPS > 12:
             # get the correct index of the start_date based on NUM_TIMESTEPS`
             # e.g. if NUM_TIMESTEPS is 36 (dekadal setup), we should take the correct
@@ -151,6 +142,7 @@ required {cls.NUM_TIMESTEPS}, got {len(timestep_positions)}"
             # TODO: 1) this needs to go into a separate function
             # 2) definition of valid_position and timestep_ind
             #  should also be changed accordingly
+            
             year = pd.to_datetime(row_d["start_date"]).year
             year_dates = pd.date_range(start=f"{year}-01-01", end=f"{year}-12-31")
             bins = pd.cut(year_dates, bins=cls.NUM_TIMESTEPS, labels=False)
