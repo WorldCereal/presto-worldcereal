7c76a1b805a2957467d9c5eb73748433a6db16fd
Butsko Christina <butskoc@sasdsnode01.vito.local>
2025-01-09 14:47:57 +0100

    formatting

--- presto/utils.py
+++ presto/utils.py
@@ -12,11 +12,22 @@ import numpy as np
 import pandas as pd
 import torch
 import xarray as xr
+
 from presto.dataops import NUM_TIMESTEPS
 
-from .dataops import (BANDS, ERA5_BANDS, MIN_EDGE_BUFFER, NODATAVALUE,
-                      NORMED_BANDS, REMOVED_BANDS, S1_BANDS, S1_S2_ERA5_SRTM,
-                      S2_BANDS, SRTM_BANDS, DynamicWorld2020_2021)
+from .dataops import (
+    BANDS,
+    ERA5_BANDS,
+    MIN_EDGE_BUFFER,
+    NODATAVALUE,
+    NORMED_BANDS,
+    REMOVED_BANDS,
+    S1_BANDS,
+    S1_S2_ERA5_SRTM,
+    S2_BANDS,
+    SRTM_BANDS,
+    DynamicWorld2020_2021,
+)
 
 # plt = None
 
